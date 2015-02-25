d07d8c810520ea31eecac6d60db958d3cce7bd71
Sroop Sunar <sroopsunar@hotmail.com>
2015-02-25 18:16:12 +0000

    Create a todos page, add a link to it in the nabber and configure the routes.

--- app/main/config/routes.rb
+++ app/main/config/routes.rb
@@ -1,6 +1,7 @@
 # See https://github.com/voltrb/volt#routes for more info on routes
 
 get '/about', _action: 'about'
+get '/todos', _action:'todos'
 
 # Routes for login and signup, provided by user-templates component gem
 get '/signup', _controller: 'user-templates', _action: 'signup'
