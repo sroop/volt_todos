9df7ff50656b300b3f58c124049901a6ea7e96cc
Sroop Sunar <sroopsunar@hotmail.com>
2015-02-26 12:44:55 +0000

    Persist data to mongodb.

--- app/main/controllers/main_controller.rb
+++ app/main/controllers/main_controller.rb
@@ -1,5 +1,7 @@
 # By default Volt generates this controller for your Main component
 class MainController < Volt::ModelController
+  model :store
+
   def index
     # Add code for when the index view is loaded
   end
@@ -9,16 +11,17 @@ class MainController < Volt::ModelController
   end
 
   def add_todo
-    page._todos << { name: page._new_todo }
+    _todos << { name: page._new_todo }
     page._new_todo = ''
   end
 
   def remove_todo(todo)
-    page._todos.delete(todo)
+    _todos.delete(todo)
   end
 
   def current_todo
-    page._todos[params._index.or(0).to_i]
+    _todos[params._index.or(0).to_i]
+  end
   end
 
   private
